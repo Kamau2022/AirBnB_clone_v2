bfa52d9d150eb9065b1ea0b312b4942bd21317a0
Benson Kamau <benkabenson@gmail.com>
2023-02-25 20:47:22 +0000

    corrected

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#this script is installing NGINX
+#this script is installing NGINX to deploy static web_static
 sudo apt-get update
 sudo apt-get install -y nginx
 sudo mkdir -p /data/
@@ -9,6 +9,7 @@ sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/hbnb_static/;}' /etc/nginx/sites-available/default
-sudo service nginx restart
+chown -R ubuntu:ubuntu /data/
+sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+service nginx restart
+
