2475832e16e3ad063c41602f2e8ed226c68028df
Benson Kamau <benkabenson@gmail.com>
2023-03-02 20:42:02 +0000

    fixed

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,13 +6,11 @@ from datetime import datetime
 
 
 def do_pack():
-    """ a function that generates .tgz archive
-    """
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
-    local("sudo mkdir -p ./versions")
-    archive = local("tar -czvf versions/web_static_{}\
-                    .tgz web_static".format(now))
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    local("mkdir -p versions")
+    archive = local("tar -cvzf versions/web_static_{}.tgz web_static/".
+                    format(time))
     if archive:
-        return ("versions/web_static_{}".format(now))
+        return ("versions/web_static_{:s}.tgz".format(time))
     else:
         return None
